4bca174d5d018d4f31633ff03a86ed07be1d607f
Eudesjs <eudsjs@gmail.com>
2024-03-17 18:18:41 -0300

    Operadores Logicos de comparação

---
+++ Fundamentos_Python/Operadores_Comparação.py
@@ -0,0 +1,42 @@
+'''
+Os operadores de comparação em Python permitem verificar se duas expressões são
+iguais, diferentes, maiores, menores, etc. São ferramentas essenciais para tomar
+decisões e realizar comparações em seus programas. Retornam com resultado
+verdadeiro (True) ou Falso (False) (tipo bool)
+'''
+# OPERADOR DE IGUALDADE ( == )
+igualdade = 3 == 4
+print(igualdade)
+
+# OPERADOR DE DIFERENÇA ( != )
+diferente = 3 != 4
+print(diferente)
+
+# OPERADOR MAIOR QUE ( > )
+maior = 3 > 4
+print(maior)
+
+# OPERADOR MENOR QUE ( < )
+menor = 3 < 4
+print(menor)
+
+# OPERADOR MAIOR OU IGUAL A ( >= )
+maior_igual = 3 >= 4
+print(maior_igual)
+
+# OPERADOR MENOR OU IGUAL A ( <= )
+menor_igual = 3 <= 4
+print(menor_igual)
+
+# OPERADOR DE IDENTIDADE ( is )
+a = 7
+b = 7.0
+print(a is b) # A é B?
+print(a == b) # A é igual a B?
+
+# OPERADOR DE IDENTIDADE ( is not )
+a = 7
+b = 7.0
+print (a is not b) # A não é B
+print(a != b) # A é diferente de B
+
