47abe77d39a4043fd81de0bfc01c4b1b17ff331f
Eudesjs <eudsjs@gmail.com>
2024-03-17 22:42:25 -0300

    Atualizando

---
+++ Exercicio_Fixação/exercicio-1.py
@@ -0,0 +1,8 @@
+'''
+Crie um programa que receba do usuário dois ou mais nomes separados
+por espaços e no mesmo input e os armazene em uma variável chamada nomes
+'''
+nomes = str(input('Informe dois ou mais nomes separados: '))
+print(nomes)
+nomes1 = nomes.split()
+print(nomes1)
